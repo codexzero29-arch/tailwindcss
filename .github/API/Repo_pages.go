4e9890ff61e5395777b26bbb1f4bcfdb00edcd5c
codexzero29-arch <265551241+codexzero29-arch@users.noreply.github.com>
2026-03-16 13:26:17 +0700

    Update and rename .github/CODEOWNERS to .github/API/Repo_pages.go

---
+++ .github/API/Repo_pages.go
@@ -0,0 +1,8 @@
+# GitHub CLI api
+# https://cli.github.com/manual/gh_api
+
+gh api \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2026-03-10" \
+  /repos/OWNER/REPO/pages
+	
