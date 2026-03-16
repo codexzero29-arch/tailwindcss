f2e6acbd63a5f80b6583ca11d0410cddb2de73fe
codexzero29-arch <265551241+codexzero29-arch@users.noreply.github.com>
2026-03-16 13:35:45 +0700

    Update Repo_pages.go

--- .github/API/Repo_pages.go
+++ .github/API/Repo_pages.go
@@ -2,7 +2,8 @@
 # https://cli.github.com/manual/gh_api
 
 gh api \
+  --method POST \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2026-03-10" \
-  /repos/OWNER/REPO/pages
-	
+  /repos/OWNER/REPO/pages \
+   -f 'source[branch]:=main' -f 'source[path]:=/docs'
